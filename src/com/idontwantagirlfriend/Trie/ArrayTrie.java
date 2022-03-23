d35881c02234081128f92de369e9d8c9de1079a5
WeLuvFD <34613980+idontwantagirlfriend@users.noreply.github.com>
2022-03-23 17:49:07 +0800

    Complete ArrayTrie class, add tests
    Implement add, remove and find methods,

--- src/com/idontwantagirlfriend/Trie/ArrayTrie.java
+++ src/com/idontwantagirlfriend/Trie/ArrayTrie.java
@@ -3,27 +3,89 @@ package com.idontwantagirlfriend.Trie;
 public class ArrayTrie {
     private Node root;
 
-    public void add(String word) {}
+    public ArrayTrie() {
+        this.root = new Node();
+    }
 
+    /**
+     * Add new {@code word} to the current trie.<br/>
+     * On null or empty value, throw IllegalArgumentException.
+     * @param word The new word in string.
+     */
+    public void add(String word) {
+        handleMeaninglessInput(word);
+        var lowercase = word.toLowerCase();
+        var cursor = root;
+        for (var i = 0; i < lowercase.length(); i++) {
+            var letter = lowercase.charAt(i);
+            handleIllegalCharacter(letter);
+            cursor.add(letter);
+            cursor = cursor.findChild(letter);
+        }
+        cursor.setIsEOW(true);
+    }
+
+    /**
+     * Remove a word from the current trie. Return the removed
+     * word if it's found, or null if the word can't be found.<br/>
+     * On null or empty value, throw IllegalArgumentException.
+     * @param word the word to be removed.
+     * @return the word, or null.
+     */
     public String remove(String word) {
-        return null;
+        handleMeaninglessInput(word);
+        var lowercase = word.toLowerCase();
+        var cursor = root;
+        for (var i = 0; i < lowercase.length() - 1; i++) {
+            var letter = lowercase.charAt(i);
+            if (!cursor.hasChild(letter)) return null;
+            cursor = cursor.findChild(letter);
+        }
+
+//        word.length() > 1 has been ensured.
+        var lastLetter = lowercase.charAt(word.length() - 1);
+        if (!cursor.hasChild(lastLetter)
+                || !cursor.findChild(lastLetter).getIsEOW())
+            return null;
+        if (!cursor.pruneChild(lastLetter))
+            cursor.findChild(lastLetter).setIsEOW(false);
+        cursor.setIsEOW(false);
+        return word;
     }
 
+    /**
+     * Find a word in the trie and return a boolean based
+     * on the result.<br/>
+     * On null or empty value, throw IllegalArgumentException.
+     * @param word the word to be found.
+     * @return the result.
+     */
     public Boolean find(String word) {
-        return false;
-    }
-
-    public int count() {
-        return 0;
+        handleMeaninglessInput(word);
+        var lowercase = word.toLowerCase();
+        var cursor = root;
+        for (var i = 0; i < lowercase.length(); i++) {
+            var letter = lowercase.charAt(i);
+            handleIllegalCharacter(letter);
+            if (!cursor.hasChild(letter)) return false;
+            cursor = cursor.findChild(letter);
+        }
+        return cursor.getIsEOW();
     }
 
     public static class Node {
+
+        public static int getLetterPosition(char letter) {
+            return letter - 'a';
+        }
+
         private final Node[] subtrees;
+        private final int ALPHABET_NUMBER = 26;
         private char letter;
         private Boolean isEOW;
 
         public Node() {
-            subtrees = new Node[26];
+            subtrees = new Node[ALPHABET_NUMBER];
             isEOW = false;
         }
 
@@ -32,30 +94,62 @@ public class ArrayTrie {
             this.letter = letter;
         }
 
-        public static int getLetterPosition(char letter) {
-            return letter - 'a';
-        }
-
         public char getLetter() {
             return letter;
+        }
+
+        public Boolean add(char letter) {
+            var success = false;
+            if (!hasChild(letter)) {
+                addChild(letter);
+                success = true;
+            }
+            return success;
+        }
+
+        public Node findChild(char letter) {
+            handleIllegalCharacter(letter);
+            return subtrees[getLetterPosition(letter)];
+        }
+
+        private Node setChild(int position, Node child) {
+            subtrees[position] = child;
+            return child;
+        }
+
+        public Boolean hasChild(char letter) {
+            return findChild(letter) != null;
+        }
+
+        public Boolean hasNoChild() {
+            for (var i = 0; i < ALPHABET_NUMBER; i++) {
+                if (subtrees[i] != null) return false;
+            }
+            return true;
+        }
+
+        public Node removeChild(char letter) {
+            if (hasChild(letter)) {
+                var removed = findChild(letter);
+                setChild(getLetterPosition(letter), null);
+                return removed;
+            }
+            return null;
         }
 
         public Node addChild(char letter) {
             handleIllegalCharacter(letter);
             var newChild = new Node(letter);
-            subtrees[getLetterPosition(letter)] = newChild;
-            return newChild;
+            return setChild(getLetterPosition(letter), newChild);
         }
 
-        public Node getChild(char letter) {
-            handleIllegalCharacter(letter);
-            return subtrees[getLetterPosition(letter)];
+        public Boolean pruneChild(char letter) {
+            if (hasChild(letter) && findChild(letter).hasNoChild()) {
+                removeChild(letter);
+                return true;
+            }
+            return false;
         }
-
-        public Boolean hasChild(char letter) {
-            return getChild(letter) != null;
-        }
-
 
         public void setIsEOW(Boolean isEOW) {
             this.isEOW = isEOW;
@@ -71,5 +165,21 @@ public class ArrayTrie {
                 throw new IllegalArgumentException(
                         "Only accept lowercase alphabetical letter.");
         }
+    }
+
+    private static void handleIllegalCharacter(char letter) {
+        var alphabeticalIndex = letter - 'a';
+        if (alphabeticalIndex < 0 || alphabeticalIndex > 26)
+            throw new IllegalArgumentException(
+                    "The word can only contains alphabetical letters.");
+    }
+
+    private void handleMeaninglessInput(String word) {
+        if (word == null)
+            throw new IllegalArgumentException(
+                    "Trie can't operate with null arguments.");
+        if (word.equals(""))
+            throw new IllegalArgumentException(
+                    "Trie can't operate with empty string.");
     }
 }
--- src/com/idontwantagirlfriend/Trie/TestArrayTrie.java
+++ src/com/idontwantagirlfriend/Trie/TestArrayTrie.java
@@ -8,10 +8,113 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class TestArrayTrie {
-    public static ArrayTrie trie;
+    public ArrayTrie trie;
 
-    public static void refreshTrie() {
+    public void refreshTrie() {
         trie = new ArrayTrie();
+    }
+
+    @BeforeEach
+    public void setUp() {
+        refreshTrie();
+    }
+
+    @Test
+    public void addAWord() {
+        trie.add("Hello");
+    }
+
+    @Test
+    public void addASpaceCharacter_ShouldThrowIllegalArgumentException() {
+        assertThrows(IllegalArgumentException.class, () -> trie.add(" "));
+    }
+
+    @Test
+    public void addEmptyString_ShouldThrowIllegalArgumentException() {
+        assertThrows(IllegalArgumentException.class, () -> trie.add(""));
+    }
+
+    @Test
+    public void addAWordThatContainsNonAlphabeticalChars_ShouldThrowIllegalArgumentException() {
+        assertThrows(IllegalArgumentException.class, () -> trie.add("Hello "));
+        assertThrows(IllegalArgumentException.class, () -> trie.add("Étranger"));
+    }
+
+    @Test
+    public void removeAWord_ShouldReturnTheRemovedWord() {
+        trie.add("Hello");
+        trie.add("world");
+        trie.add("This");
+        assertEquals("World", trie.remove("World"));
+    }
+
+    @Test
+    public void removeAWordThatDoesNotExist_ShouldReturnNull() {
+        trie.add("Hello");
+        assertNull(trie.remove("Hell"));
+        assertNull(trie.remove("A"));
+        assertNull(trie.remove("Helmet"));
+    }
+
+    @Test
+    public void removeANonExistentWordThatIsPartOfSomeWordInTheTrie_ShouldReturnNull() {
+        trie.add("fellow");
+        assertNull(trie.remove("fell"));
+    }
+
+    @Test
+    public void removeAWordThatIsPartOfSomeWordInTheTrie_ShouldReturnTheRemovedWord() {
+        trie.add("fellow");
+        trie.add("fell");
+        assertEquals("fell", trie.remove("fell"));
+    }
+
+    @Test
+    public void removeNull_ShouldThrowIllegalArgumentException() {
+        assertThrows(IllegalArgumentException.class, () -> trie.remove(null));
+        trie.add("No");
+        assertThrows(IllegalArgumentException.class, () -> trie.remove(null));
+    }
+
+    @Test
+    public void removeEmptyString_ShouldThrowIllegalArgumentException() {
+        assertThrows(IllegalArgumentException.class, () -> trie.remove(""));
+        trie.add("Aufschlag");
+        assertThrows(IllegalArgumentException.class, () -> trie.remove(""));
+    }
+
+    @Test
+    public void findAWord_ShouldReturnTrue() {
+        trie.add("ww");
+        assertTrue(trie.find("ww"));
+    }
+
+    @Test
+    public void findANonExistentWord_ShouldReturnFalse() {
+        assertFalse(trie.find("firelight"));
+        trie.add("sunken");
+        assertFalse(trie.find("firelight"));
+    }
+
+    @Test
+    public void findASimilarWord_ShouldReturnFalse() {
+        trie.add("forward");
+        assertFalse(trie.find("for"));
+        assertFalse(trie.find("forwarding"));
+    }
+
+    @Test
+    public void findEmptyString_ShouldThrowIllegalArgumentException() {
+        assertThrows(IllegalArgumentException.class, () -> trie.find(""));
+        trie.add("Abs");
+        assertThrows(IllegalArgumentException.class, () -> trie.find(""));
+    }
+
+    @Test
+    public void findNull_ShouldThrowIllegalArgumentException() {
+        assertThrows(IllegalArgumentException.class, () -> trie.find(null));
+        trie.add("Abs");
+        assertThrows(IllegalArgumentException.class, () -> trie.find(null));
     }
 
     public static class NodeTest {
@@ -34,19 +137,19 @@ public class TestArrayTrie {
         }
 
         @Test
-        public void getChildOfEmptyNode_ShouldReturnNull() {
-            assertNull(node.getChild('a'));
+        public void findChildOfEmptyNode_ShouldReturnNull() {
+            assertNull(node.findChild('a'));
         }
 
         @Test
-        public void getNonAlphabeticalChild_ShouldThrowIllegalArgumentException() {
-            assertThrows(IllegalArgumentException.class, () -> node.getChild('='));
-            assertThrows(IllegalArgumentException.class, () -> node.getChild(' '));
+        public void findNonAlphabeticalChild_ShouldThrowIllegalArgumentException() {
+            assertThrows(IllegalArgumentException.class, () -> node.findChild('='));
+            assertThrows(IllegalArgumentException.class, () -> node.findChild(' '));
         }
 
         @Test
-        public void getUppercaseLetterChild_ShouldThrowIllegalArgumentException() {
-            assertThrows(IllegalArgumentException.class, () -> node.getChild('A'));
+        public void findUppercaseLetterChild_ShouldThrowIllegalArgumentException() {
+            assertThrows(IllegalArgumentException.class, () -> node.findChild('A'));
         }
 
         @Test
@@ -87,20 +190,65 @@ public class TestArrayTrie {
         }
 
         @Test
-        public void addOneGetOne_ShouldReturnChild() {
+        public void addOneFindOne_ShouldReturnChild() {
             node.addChild('m');
-            assertInstanceOf(ArrayTrie.Node.class, node.getChild('m'));
-            assertEquals('m', node.getChild('m').getLetter());
+            assertInstanceOf(ArrayTrie.Node.class, node.findChild('m'));
+            assertEquals('m', node.findChild('m').getLetter());
         }
 
         @Test
-        public void addRepetitiveCharAndGet_ShouldReturnDifferentChildObjectEachTime() {
+        public void addRepetitiveCharAndFind_ShouldReturnDifferentChildObjectEachTime() {
             node.addChild('k');
-            var oldChild = node.getChild('k');
+            var oldChild = node.findChild('k');
             node.addChild('k');
-            var newChild = node.getChild('k');
+            var newChild = node.findChild('k');
             assertEquals(oldChild.getLetter(), newChild.getLetter());
             assertNotSame(oldChild, newChild);
+        }
+
+        @Test
+        public void emptyNodeHasNoChild_ShouldReturnTrue() {
+            assertTrue(node.hasNoChild());
+        }
+
+        @Test
+        public void addOneAndHasNoChild_ShouldReturnFalse() {
+            node.addChild('k');
+            assertFalse(node.hasNoChild());
+        }
+
+        @Test
+        public void removeANonExistentChild_ShouldReturnNull() {
+            assertNull(node.removeChild('k'));
+            node.addChild('k');
+            assertNull(node.removeChild('f'));
+        }
+
+        @Test
+        public void removeAChild_ShouldReturnTheChild() {
+            node.addChild('k');
+            var removed = node.findChild('k');
+            assertSame(removed, node.removeChild('k'));
+        }
+
+        @Test
+        public void pruneChildThatHasNoChild_ShouldReturnTrue() {
+            node.addChild('k');
+            assertTrue(node.pruneChild('k'));
+            assertNull(node.findChild('k'));
+        }
+
+        @Test
+        public void pruneChildThatHasChildren_ShouldReturnFalse() {
+            var kNode = node.addChild('k');
+            kNode.addChild('t');
+            assertFalse(node.pruneChild('k'));
+            assertSame(kNode, node.findChild('k'));
+        }
+
+        @Test
+        public void pruneNonExistentChild_ShouldReturnFalse() {
+            assertFalse(node.pruneChild('a'));
         }
 
         @Test
@@ -112,6 +260,21 @@ public class TestArrayTrie {
         public void explicitlySetIsEOWAndGetEOWStatus_ShouldReturnTrue() {
             node.setIsEOW(true);
             assertTrue(node.getIsEOW());
+        }
+
+        @Test
+        public void addOneUniqueLetter_ShouldReturnTrue() {
+            assertNull(node.findChild('a'));
+            assertTrue(node.add('a'));
+            assertEquals('a', node.findChild('a').getLetter());
+        }
+
+        @Test
+        public void addOneDuplicateLetter_ShouldReturnFalseAndKeepTheOriginalChild() {
+            var oldChild = node.addChild('k');
+            assertEquals('k', oldChild.getLetter());
+            assertFalse(node.add('k'));
+            assertSame(oldChild, node.findChild('k'));
         }
     }
 }
