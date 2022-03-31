416b5817d329f84de2bfcd934f4479e17c40814e
WeLuvFD <34613980+idontwantagirlfriend@users.noreply.github.com>
2022-03-31 14:31:59 +0800

    Create StringNode interface

---
+++ src/com/idontwantagirlfriend/Trie/StringNode.java
@@ -0,0 +1,22 @@
+package com.idontwantagirlfriend.Trie;
+
+public interface StringNode {
+    String getWord();
+
+    StringNode getChild(String substring);
+
+    Boolean contains(String substring);
+
+    StringNode remove(String substring);
+
+    Boolean add(String substring);
+
+    StringNode[] getAllChildren();
+
+    Boolean isLeaf();
+
+    void setEOW(Boolean isEOW);
+
+    Boolean getEOW();
+}
+
